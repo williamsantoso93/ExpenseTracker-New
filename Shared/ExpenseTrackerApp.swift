b3562a7ed8a7c51330b7514d7e7a90ad2eb618a3
William Santoso <williamsantoso93@yahoo.com>
2021-12-19 19:00:49 +0700

    Initial Commit

---
+++ Shared/ExpenseTrackerApp.swift
@@ -0,0 +1,20 @@
+//
+//  ExpenseTrackerApp.swift
+//  Shared
+//
+//  Created by Ruangguru on 19/12/21.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
