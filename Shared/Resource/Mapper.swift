a1d9a4f4b48160de4da2c40e9ddbae213969a2e8
William Santoso <william.santoso@ruangguru.com>
2022-01-10 09:33:10 +0700

    add coredata mapper function

--- Shared/Resource/Mapper.swift
+++ Shared/Resource/Mapper.swift
@@ -6,8 +6,11 @@
 //
 
 import Foundation
+import CoreData
 
 struct Mapper {
+    static let viewContext = CoreDataManager.shared.viewContext
+    
     //MARK: - YearMonthModel
     static func mapYearMonthListRemoteToLocal(_ remote: [ResultProperty<YearMonthProperty>]) -> [YearMonthModel] {
         remote.map { result in
@@ -42,6 +45,40 @@ struct Mapper {
         )
     }
     
+    static func mapYearMonthListCoreDataToLocal(_ coreData: [YearMonth]) -> [YearMonthModel] {
+        coreData.map { result in
+            yearMonthListCoreDataToLocal(result)
+        }
+    }
+    
+    static func yearMonthListCoreDataToLocal(_ coreData: YearMonth) -> YearMonthModel {
+        YearMonthModel(
+            id: coreData.id?.uuidString ?? "",
+            name: coreData.name ?? "",
+            month: coreData.month ?? "",
+            year: coreData.year ?? "",
+            totalIncomes: 0,
+            totalExpenses: 0
+        )
+    }
+    
+    static func mapYearMonthLocalToCoreData(_ local: [YearMonthModel]) -> [YearMonth] {
+        local.map { result in
+            yearMonthLocalToCoreData(result)
+        }
+    }
+    
+    static func yearMonthLocalToCoreData(_ local: YearMonthModel) -> YearMonth {
+        let yearMonth = YearMonth(context: viewContext)
+        
+        yearMonth.id = UUID(uuidString: local.id)
+        yearMonth.name = local.name
+        yearMonth.year = local.year
+        yearMonth.month = local.month
+        
+        return yearMonth
+    }
+    
     //MARK: - Expense
     static func mapExpenseRemoteToLocal(_ remote: [ResultProperty<ExpenseProperty>]) -> [ExpenseModel] {
         remote.map { result in
@@ -65,15 +102,6 @@ struct Mapper {
         )
     }
     
-    static func multiSelectsToStrings(_ multiSelects: [Select]?) -> [String]? {
-        guard let multiSelects = multiSelects else { return nil }
-        guard !multiSelects.isEmpty else { return nil }
-        
-        return multiSelects.map { result in
-            result.name
-        }
-    }
-    
     static func mapExpenseLocalToRemote(_ local: [ExpenseModel]) -> [ExpenseProperty] {
         local.map { result in
             expenseLocalToRemote(result)
@@ -94,15 +122,47 @@ struct Mapper {
         )
     }
     
-    static func stringsToMultiSelects(_ strings: [String]?) -> [Select]? {
-        guard let strings = strings else { return nil }
-        guard !strings.isEmpty else { return nil }
-        
-        let map = strings.map { result in
-            Select(name: result)
+    static func mapEpxensesCoreDataToLocal(_ coreData: [Expense]) -> [ExpenseModel] {
+        coreData.map { result in
+            epxenseCoreDataToLocal(result)
         }
+    }
+    
+    static func epxenseCoreDataToLocal(_ coreData: Expense) -> ExpenseModel {
+        ExpenseModel(
+            blockID: coreData.id?.uuidString ?? "",
+            id: coreData.id?.uuidString ?? "",
+            yearMonth: "",
+            note: coreData.note,
+            value: Int(coreData.value),
+            duration: coreData.duration,
+            paymentVia: coreData.paymentVia,
+            store: coreData.store,
+            types: coreData.types?.split(),
+            date: coreData.date,
+            keywords: ""
+        )
+    }
+    
+    static func mapEpxensesLocalToCoreData(_ local: [ExpenseModel]) -> [Expense] {
+        local.map { result in
+            epxenseLocalToCoreData(result)
+        }
+    }
+    
+    static func epxenseLocalToCoreData(_ local: ExpenseModel) -> Expense {
+        let expense = Expense(context: viewContext)
         
-        return map
+        expense.id = UUID(uuidString: local.id)
+        expense.note = local.note
+        expense.types = local.types?.joinedWithCommaNoSpace()
+        expense.value = Int64(local.value ?? 0)
+        expense.duration = local.duration
+        expense.paymentVia = local.paymentVia
+        expense.store = local.store
+        expense.date = local.date
+        
+        return expense
     }
     
     //MARK: - Income
@@ -142,6 +202,43 @@ struct Mapper {
         )
     }
     
+    static func mapIncomesCoreDataToLocal(_ coreData: [Income]) -> [IncomeModel] {
+        coreData.map { result in
+            incomeCoreDataToLocal(result)
+        }
+    }
+    
+    static func incomeCoreDataToLocal(_ coreData: Income) -> IncomeModel {
+        IncomeModel(
+            blockID: coreData.id?.uuidString ?? "",
+            id: coreData.id?.uuidString ?? "",
+            yearMonth: "",
+            value: Int(coreData.value),
+            types: coreData.types?.split(),
+            note: coreData.note,
+            date: coreData.date,
+            keywords: ""
+        )
+    }
+    
+    static func mapIncomesLocalToCoreData(_ local: [IncomeModel]) -> [Income] {
+        local.map { result in
+            incomeLocalToCoreData(result)
+        }
+    }
+    
+    static func incomeLocalToCoreData(_ local: IncomeModel) -> Income {
+        let income = Income(context: viewContext)
+        
+        income.id = UUID(uuidString: local.id)
+        income.note = local.note
+        income.types = local.types?.joinedWithCommaNoSpace()
+        income.value = Int64(local.value ?? 0)
+        income.date = local.date
+        
+        return income
+    }
+    
     //MARK: - Type
     static func mapTypeRemoteToLocal(_ remote: [ResultProperty<TypeProperty>]) -> [TypeModel] {
         remote.map { result in
@@ -169,6 +266,38 @@ struct Mapper {
             name: TitleProperty(title: [Title(text: TextContent(content: local.name))]),
             category: SingleSelectProperty(select: Select(name: local.category))
         )
+    }
+    
+    static func mapTypesCoreDataToLocal(_ coreData: [TypeData]) -> [TypeModel] {
+        coreData.map { result in
+            typeCoreDataToLocal(result)
+        }
+    }
+    
+    static func typeCoreDataToLocal(_ coreData: TypeData) -> TypeModel {
+        TypeModel(
+            blockID: coreData.id?.uuidString ?? "",
+            name: coreData.name ?? "",
+            category: coreData.category ?? "",
+            keywords: ""
+        )
+    }
+    
+    
+    static func mapTypesLocalToCoreData(_ local: [TypeModel]) -> [TypeData] {
+        local.map { result in
+            typeLocalToCoreData(result)
+        }
+    }
+    
+    static func typeLocalToCoreData(_ local: TypeModel) -> TypeData {
+        let type = TypeData(context: viewContext)
+        
+        type.id = UUID(uuidString: local.blockID)
+        type.name = local.name
+        type.category = local.category
+        
+        return type
     }
     
     //MARK: - Setting
@@ -210,6 +339,48 @@ struct Mapper {
         )
     }
     
+    static func mapTemplateCoreDataToLocal(_ coreData: [Template]) -> [TemplateModel] {
+        coreData.map { result in
+            templateCoreDataToLocal(result)
+        }
+    }
+    
+    static func templateCoreDataToLocal(_ coreData: Template) -> TemplateModel {
+        TemplateModel(
+            blockID: coreData.id?.uuidString ?? "",
+            name: coreData.name,
+            category: coreData.category,
+            duration: coreData.duration,
+            paymentVia: coreData.paymentVia,
+            store: coreData.store,
+            types: coreData.types?.split(),
+            value: Int(coreData.value),
+            keywords: ""
+        )
+    }
+    
+    static func mapTemplatesLocalToCoreData(_ local: [TemplateModel]) -> [Template] {
+        local.map { result in
+            templateLocalToCoreData(result)
+        }
+    }
+    
+    static func templateLocalToCoreData(_ local: TemplateModel) -> Template {
+        let template = Template(context: viewContext)
+        
+        template.id = UUID(uuidString: local.blockID)
+        template.name = local.name
+        template.category = local.category
+        template.types = local.types?.joinedWithCommaNoSpace()
+        template.value = Int64(local.value ?? 0)
+        template.duration = local.duration
+        template.paymentVia = local.paymentVia
+        template.store = local.store
+        
+        return template
+    }
+    
+    //MARK: - Error Message
     static func errorMessageRemoteToLocal(_ remote: ErrorResponse) -> ErrorMessage {
         ErrorMessage(
             title: remote.code,
@@ -218,6 +389,26 @@ struct Mapper {
     }
     
     //MARK: - To Property
+    static func multiSelectsToStrings(_ multiSelects: [Select]?) -> [String]? {
+        guard let multiSelects = multiSelects else { return nil }
+        guard !multiSelects.isEmpty else { return nil }
+        
+        return multiSelects.map { result in
+            result.name
+        }
+    }
+    
+    static func stringsToMultiSelects(_ strings: [String]?) -> [Select]? {
+        guard let strings = strings else { return nil }
+        guard !strings.isEmpty else { return nil }
+        
+        let map = strings.map { result in
+            Select(name: result)
+        }
+        
+        return map
+    }
+    
     static func textToSingleSelectProperty(_ text: String?) -> SingleSelectProperty? {
         guard let text = text else {
             return nil
