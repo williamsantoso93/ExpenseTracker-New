2d97f27fc5a4ab0b17007af2259ccfd73248efe1
William Santoso <william.santoso@ruangguru.com>
2022-01-15 20:19:58 +0700

    readd hide keybaord

---
+++ Shared/Resource/HideKeyboardAppWide.swift
@@ -0,0 +1,23 @@
+//
+//  HideKeyboardAppWide.swift
+//  Otospector DealerApp
+//
+//  Created by William Santoso on 15/06/21.
+//
+
+import SwiftUI
+
+extension UIApplication: UIGestureRecognizerDelegate {
+    func addTapGestureRecognizer() {
+        guard let window = windows.first else { return }
+        let tapGesture = UITapGestureRecognizer(target: window, action: #selector(UIView.endEditing))
+        tapGesture.cancelsTouchesInView = false
+        tapGesture.delegate = self
+        tapGesture.name = "MyTapGesture"
+        window.addGestureRecognizer(tapGesture)
+    }
+
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return false // set to `false` if you don't want to detect tap during other gestures
+    }
+}
